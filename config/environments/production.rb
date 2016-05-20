13e726270962e60c84cc61c210cf587ff9d21d51
Thomas Carli <thomas@procore.com>
2016-05-20 06:55:25 +0000

    add precompile config to production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -14,6 +14,11 @@ Rails.application.configure do
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
 
+  config.assets.precompile << Proc.new { |path|
+  if path =~ /\.(eot|svg|ttf|woff)\z/
+    true
+  end
+
   # Enable Rack::Cache to put a simple HTTP cache in front of your application
   # Add `rack-cache` to your Gemfile before enabling this.
   # For large-scale production use, consider using a caching reverse proxy like
